62ca4a0958c34b29a8d2decf6f2c4612c52fff93
Lilanka Ravinath <inbox.lilanka@gmail.com>
2020-05-10 18:45:54 +0530

    create the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-td-forms';
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
 }
